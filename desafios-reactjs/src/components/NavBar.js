6f566cbd970a94e31fa10a4a24051216614d082b
Mauro Fitz Maurice <maurofitzmaurice@gmail.com>
2022-06-18 18:13:46 -0300

    agregado node - navbar

---
+++ desafios-reactjs/src/components/NavBar.js
@@ -0,0 +1,34 @@
+import React from "react"
+
+const menuItems = [
+    {
+      id: "1",
+      label: "Home",
+    },
+    {
+      id: "2",
+      label: "About",
+    },
+    {
+      id: "3",
+      label: "Shop",
+    },
+    {
+      id: "4",
+      label: "Contact",
+    },
+  ];
+
+  const NavBar = () => {
+    return (
+        <div className="nav">
+            <h1 className="nav-logo">ESHOP</h1>
+            <div>
+                {menuItems.map((item) => (
+                <a href="/" className="nav-item" key={menuItems.id}>{item.label}</a>
+              ))}
+            </div>
+        </div>
+    );
+  };
+  export default NavBar;
