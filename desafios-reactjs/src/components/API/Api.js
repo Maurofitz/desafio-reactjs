df1b7a7c2f3698cae4d17b854473abc3a7c4a880
Mauro Fitz Maurice <maurofitzmaurice@gmail.com>
2022-06-29 22:14:54 -0300

    promise terminado

--- desafios-reactjs/src/components/API/Api.js
+++ desafios-reactjs/src/components/API/Api.js
@@ -1,17 +1,17 @@
-import producto1 from "../../Assets/product-01.jpg"
-import producto2 from "../../Assets/product-02.jpg"
-import producto3 from "../../Assets/product-03.jpg"
-import producto4 from "../../Assets/product-04.jpg"
-import producto5 from "../../Assets/product-05.jpg"
-import producto6 from "../../Assets/product-07.jpg"
+import producto1 from "../../Assets/producto1.jpg"
+import producto2 from "../../Assets/producto2.jpg"
+import producto3 from "../../Assets/producto3.jpg"
+import producto4 from "../../Assets/producto4.jpg"
+import producto5 from "../../Assets/producto5.jpg"
+import producto6 from "../../Assets/producto6.jpg"
 
 const products = [
-    {id:'01', name:'Remera Basica',  img: {producto1}, stock:10},
-    {id:'02', name:'Camisa blanca',  img:{producto2}, stock:8},
-    {id:'03', name:'Camisa',  img:{producto3}, stock:7},
-    {id:'04', name:'Tapado',  img:{producto4}, stock:50},
-    {id:'05', name:'Camisa jean',  img:{producto5}, stock:2},
-    {id:'06', name:'Blazer',  img:{producto6}, stock:79},
+    {id:'01', name:'Remera Basica',  image: producto1, stock:10},
+    {id:'02', name:'Camisa blanca',  image:producto2, stock:8},
+    {id:'03', name:'Camisa',  image:producto3, stock:7},
+    {id:'04', name:'Tapado',  image:producto4, stock:50},
+    {id:'05', name:'Camisa jean',  image:producto5, stock:2},
+    {id:'06', name:'Blazer',  image:producto6, stock:79},
   ]
 
   export const getData = new Promise ((resolve, reject) =>{
--- desafios-reactjs/src/components/Item/Item.js
+++ desafios-reactjs/src/components/Item/Item.js
@@ -1,17 +1,105 @@
 import React from 'react'
 
-
 const Item = ({product}) => {
-  const {img, name}= product
-
+  const {image, name}= product
+console.log (image)
   return (
     <section class="ftco-section bg-light">
     	<div class="container">
+				<div class="row justify-content-center mb-3 pb-3">
+          <div class="col-md-12 heading-section text-center ftco-animate">
+            {/* <h2 class="mb-4">nueva coleccion</h2>   */}
+          </div>
+        </div>   		
+    	</div>
+    	<div class="container">
     		<div class="row">
-    			<div class="col-md-4">
+    			<div class="col-sm col-md-6 col-lg ftco-animate">
+    				<div class="product">
+    					<a href="#" class="img-prod"> <img class="img-fluid" src={image} alt="Colorlib Template"/>
+    						<div class="overlay"></div>
+    					</a>
+    					<div class="text py-3 px-3">
+    						<h3><a href="#">{name}</a></h3>
+    						<div class="d-flex">
+    							<div class="pricing">
+		    						<p class="price"><span class="mr-2 price-dc">$120.00</span><span class="price-sale">$80.00</span></p>
+		    					</div>
+	    					</div>
+	    					<p class="bottom-area d-flex px-3">
+							<p class="d-flex mb-0 d-block"><a href="#" class="btn btn-primary py-2 mr-1">Ver mas</a> <a href="index.html" class="btn btn-secondary py-2 ml-1">Comprar</a></p>
+    						</p>
+    					</div>
+    				</div>
+    			</div>
+    			<div class="col-sm col-md-6 col-lg ftco-animate">
+    				<div class="product">
+    					<a href="#" class="img-prod"><img class="img-fluid" src={image} alt="Colorlib Template"/>
+    						<div class="overlay"></div>
+    					</a>
+    					<div class="text py-3 px-3">
+    						<h3><a href="#">{name}</a></h3>
+    						<div class="d-flex">
+    							<div class="pricing">
+		    						<p class="price"><span>$120.00</span></p>
+		    					</div>	
+	    					</div>
+    						<p class="bottom-area d-flex px-3">
+							<p class="d-flex mb-0 d-block"><a href="#" class="btn btn-primary py-2 mr-1">Ver mas</a> <a href="index.html" class="btn btn-secondary py-2 ml-1">Comprar</a></p>
+    						</p>
+    					</div>
+    				</div>
+    			</div>
+    			<div class="col-sm col-md-6 col-lg ftco-animate">
+    				<div class="product">
+    					<a href="#" class="img-prod"><img class="img-fluid" src={image} alt="Colorlib Template"/>
+	    					<div class="overlay"></div>
+	    				</a>
+    					<div class="text py-3 px-3">
+    						<h3><a href="#">{name}</a></h3>
+    						<div class="d-flex">
+    							<div class="pricing">
+		    						<p class="price"><span>$23420.00</span></p>
+		    					</div>	
+	    					</div>
+    						<p class="bottom-area d-flex px-3">
+							<p class="d-flex mb-0 d-block"><a href="#" class="btn btn-primary py-2 mr-1">Ver mas</a> <a href="index.html" class="btn btn-secondary py-2 ml-1">Comprar</a></p>
+    						</p>
+    					</div>
+    				</div>
+    			</div>
+    			<div class="col-sm col-md-6 col-lg ftco-animate">
+    				<div class="product">
+    					<a href="#" class="img-prod"><img class="img-fluid" src={image} alt="Colorlib Template"/>
+    						<div class="overlay"></div>
+    					</a>
+    					<div class="text py-3 px-3">
+    						<h3><a href="#">{name}</a></h3>
+    						<div class="d-flex">
+    							<div class="pricing">
+		    						<p class="price"><span>$13420.00</span></p>
+		    					</div>	
+	    					</div>
+    						<p class="bottom-area d-flex px-3">
+    							<p class="d-flex mb-0 d-block"><a href="#" class="btn btn-primary py-2 mr-1">Ver mas</a> <a href="index.html" class="btn btn-secondary py-2 ml-1">Comprar</a></p>
+    						</p>
+    					</div>
+    				</div>
+    			</div>
+    		</div>
+    	</div>
+    </section>
+  )
+};
+
+export default Item;
+{/* <section class="ftco-section bg-light">
+    	<div class="container">
+    		<div class="row">
+    			<div class="col-md-3">
     				<div class="ftco-animate">
     					<div class=" d-flex align-items-end">
-							<img src={img} alt="Foto Producto" />
+							<img class="card-photo" src={image} alt="Foto Producto" />
     					</div>
     					<div class="text">
     						<h2 class="mb-0"><a>{name}</a></h2>
@@ -24,8 +112,4 @@ const Item = ({product}) => {
     			</div>
         </div>
       </div>
-    </section>
-  )
-};
-
-export default Item;
+    </section> */}
